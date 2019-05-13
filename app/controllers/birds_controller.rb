0ae9ed8fd3143868c1a84dd94316d4af5c3f66c5
maxwellbenton <maxwellbenton@gmail.com>
2019-05-13 18:41:01 -0400

    resets

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,6 @@
 class BirdsController < ApplicationController
-    def index
-      @birds = Bird.all
-      render 'birds/index.html.erb'
-      end
-  end
+  def index
+    @birds = Bird.all
+    render 'birds/index.html.erb'
+  end
+end
